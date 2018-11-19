cca38be585c962c0b77e34fb5d59a1d5065cf191
kywkyw01 <43123060+kywkyw01@users.noreply.github.com>
2018-11-19 09:02:58 +0900

    Revert "Revert "pyqt_practice file""
    This reverts commit 6c1826d4b085e10dce4cafd177a6310692454459.

---
+++ pyqt_practice/pyqt_prac_2.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox
+from PyQt5.QtCore import QCoreApplication
+
+class Exam(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    def initUI(self):
+        btn = QPushButton("push me!", self)
+        btn.resize(btn.sizeHint())  # sizeHint : 글씨를 기준으로 적당히 크기를 조절
+        btn.move(50, 50)
+        btn.clicked.connect(QCoreApplication.instance().quit)  # <Click>이라는 Method와 탭을 닫는 함수를 Connect
+        self.resize(500, 500)
+        self.setWindowTitle("second")
+        self.show()
+    def closeEvent(self, QCloseEvent):
+        print('tab closed')  # 탭 닫기 시도시에 'tab closed'라는 메시지 출력
+        ans = QMessageBox.question(self, 'Quit.', 'Quit?',
+        QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)  # 종료 키를 눌렀을 때 재확인 창 출력. Yes 선택시 ans == QMessageBox.Yes
+        if ans is QMessageBox.Yes:
+            QCloseEvent.accept()
+        else:
+            QCloseEvent.ignore()
+
+
+app = QApplication(sys.argv)
+w = Exam()
+sys.exit(app.exec_())
