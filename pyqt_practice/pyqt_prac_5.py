6c1826d4b085e10dce4cafd177a6310692454459
kywkyw01 <43123060+kywkyw01@users.noreply.github.com>
2018-11-19 09:02:37 +0900

    Revert "pyqt_practice file"
    This reverts commit bf88cdaec2af80758de07f7398c0ff18fd5df335.

--- pyqt_practice/pyqt_prac_5.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication
-
-class Example(QWidget) :
-    def __init__(self) :
-        super().__init__()
-        self.initUI()
-    def initUI(self) :
-        pass
