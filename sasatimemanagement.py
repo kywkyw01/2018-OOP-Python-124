e4fe7471a660d2c259e986d160d9696d5353ddd9
jazzdosa <44857875+jazzdosa@users.noreply.github.com>
2018-11-22 14:05:58 +0900

    개발방식

--- sasatimemanagement.py
+++ sasatimemanagement.py
@@ -1,5 +1,13 @@
 import datetime
 
 class sctable:
-    def __init__(self):
-        self.sctab=[['empty' for j in range(7)] for i in range 14]
+    def __init__(self, sctab = ''):
+        self.sctab=[['empty' for j in range(7)] for i in range(14)]
+
+todaysc=sctable()
+todaysc.sctab[0][5]=1
+for i in todaysc.sctab:
+    for j in i:
+        print(j, end=" ")
+    print("")
+tomorrow=sctable()
