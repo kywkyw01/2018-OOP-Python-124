cc3731aa9d26af22b967666d78a9a782065a1813
danune <sbico@sasa.hs.kr>
2018-11-28 03:06:42 +0900

    Result Time Table

--- project_GUI/result_table.py
+++ project_GUI/result_table.py
@@ -180,7 +180,7 @@ class Ui_MainWindow(object):
         self.pushButton.setText(_translate("MainWindow", "Top"))
         self.pushButton_2.setText(_translate("MainWindow", "Bottom"))
 
-
+# top bottom 기능 추가 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
@@ -189,4 +189,3 @@ if __name__ == "__main__":
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
